dec411ff32a1cb50608e940271cf92d6276ba3f0
KumarNavin <navins398@gmail.com>
2023-01-06 22:29:53 +0530

    uc3 WELCOME

--- src/com/bridgelabz/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToeGame.java
@@ -6,9 +6,10 @@ import java.util.Scanner;
 public class TicTacToeGame {
     public static char inputUser;
     public static char inputC;
+    public static char[] board = new char[10];
 
     public static void main(String[] args) {
-        char[] board = new char[10];
+
         for (int i = 1; i < 10; i++) {
             board[i] = ' ';
         }
@@ -17,7 +18,7 @@ public class TicTacToeGame {
             inputC = 'O';
         } else {
             inputC = 'X';
-        }
+        }TicTacToeGame.showBoard();
     }
 
     public static char input() {
@@ -25,5 +26,13 @@ public class TicTacToeGame {
         Scanner in = new Scanner(System.in);
         char inputU = in.next().charAt(0);
         return inputU;
+    }
+
+    public static void showBoard() {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("---------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("---------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
     }
 }
